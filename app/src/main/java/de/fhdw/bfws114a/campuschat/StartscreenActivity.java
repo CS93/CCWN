fdeda6ffaab75050f9503bf0f1b428592d882fd1
CS93 <Carsten.Schlender@web.de>
2016-04-15 16:50:38 +0200

    Start!

---
+++ app/src/main/java/de/fhdw/bfws114a/campuschat/StartscreenActivity.java
@@ -0,0 +1,13 @@
+package de.fhdw.bfws114a.campuschat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartscreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startscreen);
+    }
+}
