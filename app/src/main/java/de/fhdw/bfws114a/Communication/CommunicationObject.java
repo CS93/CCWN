9fccaa1b0a7ee3d950de6897edfd05bc43d7c8d4
techcom1994 <ricardo@la-valle.de>
2016-04-28 12:39:00 +0200

    Implementieren von testmethoden im Communication Object zwecks Test vom JsonParser

--- app/src/main/java/de/fhdw/bfws114a/Communication/CommunicationObject.java
+++ app/src/main/java/de/fhdw/bfws114a/Communication/CommunicationObject.java
@@ -1,12 +1,20 @@
 package de.fhdw.bfws114a.Communication;
 
+import de.fhdw.bfws114a.dataInterface.JsonParser;
+
 /**
  * Created by Carsten on 27.04.2016.
  */
 public class CommunicationObject {
 
+    JsonParser mJsonParser;
+
+    public CommunicationObject(){
+        mJsonParser = new JsonParser();
+    }
+
 
     public void sendMessage(String text) {
-
+        mJsonParser.test();
     }
 }
